842a3d3bed902e3e1a40340b09a094f1cc94dc56
hzsenko <hzsenko@mail.ru>
2019-10-29 12:52:44 +0700

    add scss,add fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './App.scss';
 
 function App() {
   return (
     <div className="app">
-
+      <div className="app-title">
+        Калькулятор
+      </div>
     </div>
   );
 }
